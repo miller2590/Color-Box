d5595a5b45cbb9880d15a9a6574d8ba53ac26630
miller2590 <miller2590@gmail.com>
2021-11-28 17:24:24 -0600

    initial commit

---
+++ src/BoxList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+class BoxList extends React.Component {
+    render() {
+        return(
+            <h1>Hi There</h1>
+        )
+    }
+}
+
+export default BoxList
